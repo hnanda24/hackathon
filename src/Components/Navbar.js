3fabb0c7f901d6aa8d1401f82d749bfcd6863fbf
sarthak783891 <gsarthak783891@gmail.com>
2023-10-06 03:22:18 +0530

    navbar fixed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,40 +1,45 @@
 import React from "react"
 import "../assets/Navbar.css"
 
-export default function Navbar(){
+export default function Navbar() {
 
     return (
         <div>
             <nav className="navbar">
 
-                <img src="yoga.jpg" className="image"></img>
+                <div className="navItems">
+                    <img src="yoga.jpg" className="image"></img>
+                    <h1 className="heading"> ManoYog </h1>
+                </div>
+                <div className="navItems">
 
-                <h1 className="heading"> ManoYog </h1>
+                    <h4>
+                        <a href="">Home</a>
+                    </h4>
 
-                <h4>
-                    <a href="">Home</a>
-                </h4>
+                    <h4>
+                        <a href="">Programs</a>
+                    </h4>
 
-                <h4>
-                    <a href="">Programs</a>
-                </h4>
+                    <h4 className="leftItems">
+                        <a href="">Download App</a>
+                    </h4>
+                </div>
+                <div className="navItems">
 
-                <h4 className="leftItems">
-                    <a href="">Download App</a>
-                </h4>
-
-                    <h4 className="rightNavItems">
+                    <h4 >
                         <button className="log">Login</button>
                     </h4>
 
-                    <h4 className="rightNavItems">
+                    <h4 >
                         <button className="reg">Sign Up</button>
                     </h4>
+                </div>
 
             </nav>
 
 
         </div>
-        
+
     )
 }
