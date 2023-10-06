a00cb4f881bde4e68f3ede891fb234de17e833f0
Hardik <hnanda005@gmail.com>
2023-10-06 09:25:20 +0530

    all done

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Main from './Components/Main'
 import Card from './Components/Card'
-import MoodTracker from './Components/MoodTracker'
 const HomePage = () => {
   return (
     <>
     <Main/>
     <Card/>
-    <MoodTracker/>
     </>
   )
 }
